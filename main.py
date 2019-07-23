8aa432d35ca7ffabff3214dc8e984ea9ef138e02
MrBizWiz3494 <wilsonis123@icloud.com>
2019-07-23 15:58:44 -0500

    Added remaining functionality as requested

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from discord.ext.commands import has_permissions
 
 import json
 
-import config
 import ztoken
 
-bot = commands.Bot(command_prefix = "1qaz")
+bot = commands.Bot(command_prefix = "!")
 extensions = ["cogs.SetMessages"] # list of cogs to call
 
 @bot.event
@@ -18,12 +17,12 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-	channel = bot.get_channel(603028536546164751) 
+	channel = bot.get_channel(588438854738903122) 
 	await channel.send(f"{member.mention}, are you a high ranked member of an allied clan and want to be an ally? Or would you like to join the clan.\n *Enter ally or join*")
 
 
 async def response(message):
-	if message.author.id != 603027473151819786 and message.channel.id == 603028536546164751:
+	if message.author.id != 603027473151819786 and message.channel.id == 588438854738903122:
 		with open(r"C:\Users\wilso\Desktop\1ARC-Recruitment-Bot\config.json", 'r') as f:
 			config = json.load(f)
 
@@ -35,13 +34,15 @@ async def response(message):
 		author = message.author
 		#####
 		if message.content.lower() == "join":
+			role = discord.utils.get(message.guild.roles, name = "Potential Recruit")
+			await author.add_roles(role)
 			await channel.send(f"{author.mention}, {joinMsg}")
 
 		elif message.content.lower() == "ally":
 			#await self.msg.delete()
-			role = discord.utils.get(message.guild.roles, name = "Unverified Ally")
+			role = discord.utils.get(message.guild.roles, name = "Unverified Allies")
 			await author.add_roles(role)
-			channel = bot.get_channel(603037936161652746) 
+			channel = bot.get_channel(603036173622837279) 
 			await channel.send(f"{author.mention}, {allyMsg}")
 
 	#if message.channel.id == 603037936161652746
